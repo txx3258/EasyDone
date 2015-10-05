dc16f03cce35e4e587df79b86f2115cd2b523633
txx3258 <soft_tangxiaoxian@163.com>
2015-10-05 17:45:30 +0800

    direct

--- routes/index.js
+++ routes/index.js
@@ -41,7 +41,7 @@ router.get('/pingpp',function(req,res,next){
   var channel= "alipay_pc_direct";//req.query.channel,
       amount=req.query.amount;
 
-  var pingpp = require('pingpp')('sk_live_PefzrLmjLOaP5aLSOObzLWDK');
+  var pingpp = require('pingpp')('sk_test_n5Cmj5GGqDm98SKaPKejLafH');
 // pingpp.parseHeaders(/*headers*/); // 把从客户端传上来的 Headers 传到这里
 
   var extra = {};
